b4bbe56971262cc1bbc40f6e3693e4cb6375c2d1
YOGESH JAISANKAR <yogeshjaisnkar@gmail.com>
2023-06-13 12:31:54 +0530

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:netflix_clone/SplashScreen/splash.dart';
-import 'package:netflix_clone/screens/Home.dart';
-import 'package:netflix_clone/screens/Onboard/onboard_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
